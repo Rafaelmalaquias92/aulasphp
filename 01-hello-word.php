ec974f474a6fd2c4bb4945f230e8c8410234dfc7
Rafaelmalaquias92 <rafaelmalaquias92@gmail.com>
2024-10-15 16:28:05 -0300

    Create 01-hello-word.php

---
+++ 01-hello-word.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iniciando com PHP</title>
+</head>
+<body>
+    <h1>Experiências com PHP</h1>
+    <hr>
+    <?php
+    echo "Ola";
+    echo "<p>Programador Web</p>";
+    echo "<p style='color:red'>Senac Penha</p>";
+    echo "<p style=\"color:red\">Senac Penha</p>";
+    ?>
+
+</body>
+</html>
