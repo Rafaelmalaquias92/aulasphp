3267fdba2a513f41ecd3267177ff41af1c2e3694
Rafaelmalaquias92 <rafaelmalaquias92@gmail.com>
2024-11-05 15:27:08 -0300

    eventos

--- javascript/script5.js
+++ javascript/script5.js
@@ -15,8 +15,26 @@ Exemplos: acessando/manipulando o DOM
 
 const subtitulo = document.getElementById("subtitulo-dom");
 const titulo = document.querySelector("h1");
-const varioselementos = document.querySelectorAll("h3","a","hr");
+const varioselementos = document.querySelectorAll("h3,a,hr");
+const descricaoDom = document.querySelector(".descricao-dom");
 
 console.log(subtitulo);
 console.log(titulo);
 console.log(varioselementos);
+
+
+
+
+
+// CSS inline (CSS in JS)
+subtitulo.style.backgroundColor = "red";
+
+// CSS atraves de classes via JS
+descricaoDom.classList.add("destaque");
+
+// Manipulação de Eventos
+
+const pagina = document.querySelector("body");
+const exemplo1 = document.querySelector("#exemplo1");
+const mensagem  = document.querySelector("#mensagem");
+
